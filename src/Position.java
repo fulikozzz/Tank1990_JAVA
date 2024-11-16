05f4e5c3a5c93742b8664c0ac0f3dbe51ff03fb5
fulik0z <fulikozzz@gmail.com>
2024-11-16 22:11:09 +0700

    Добавлен класс Position

--- src/Position.java
+++ src/Position.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
-
 public class Position {
+    private int pos_x;
+    private int pos_y;
+
+    public Position() {
+        this.pos_x = 0;
+        this.pos_y = 0;
+    }
+
+    public Position(int x, int y) {
+        this.pos_x = x;
+        this.pos_y = y;
+    }
+
+    public int Get_PosX() { return this.pos_x; }
+
+    public int Get_PosY() { return this.pos_y; }
+
+    public void Set_PosX (int x) { this.pos_x = x; }
+
+    public void Set_PosY (int y) { this.pos_y = y; }
 }
